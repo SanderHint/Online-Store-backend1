32169b8159c30d0f009d3c7f6d0940b552674a7a
User <you@example.com>
2023-03-21 22:34:03 +0200

    first commit of Final project backend

---
+++ src/main/java/com/example/onlinestorebackend1/OnlineStoreBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.onlinestorebackend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreBackend1Application.class, args);
+	}
+
+}
